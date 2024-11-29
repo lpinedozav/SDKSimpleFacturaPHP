7923ea3054462caa8f392bec3e3bd897c91fde30
carlosperea14 <carlos.perea@inacapmail.cl>
2024-11-29 11:32:33 -0300

    se termino lo del folio

--- src/index.php
+++ src/index.php
@@ -927,3 +927,23 @@ if ($response->Status === 200) {
     echo "Error ({$response->Status}): {$response->Message}\n";
     print_r($response->Errors);
 }
+
+
+$request = new SolicitudFoliosRequest(
+    rutEmpresa: '76269769-6',
+    tipoDTE: 33,
+    ambiente: 0
+);
+
+
+$response = $client->Folio->FoliosSinUsoAsync($request)->wait();
+
+if ($response->Status === 200) {
+    echo 'Status: ' . $response->Status . "\n";
+    echo "Message: {$response->Message}\n";
+    echo "Datos de la Respuesta:\n";
+    print_r($response->Data);
+} else {
+    echo "Error ({$response->Status}): {$response->Message}\n";
+    print_r($response->Errors);
+}
