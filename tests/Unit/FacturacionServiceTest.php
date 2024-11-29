fff20daab24e7f928415fbfbc2429fb966b76a26
carlosperea14 <carlos.perea@inacapmail.cl>
2024-11-29 13:48:42 -0300

    se hizo el primer test

--- tests/Unit/FacturacionServiceTest.php
+++ tests/Unit/FacturacionServiceTest.php
@@ -49,7 +49,32 @@ class FacturacionServiceTest extends TestCase
         $this->assertEquals(200, $result->Status, 'El estado de la respuesta debe ser 200.');
         $this->assertNotNull($result->Data, 'Los datos del PDF no deben ser nulos.');
         $this->assertIsString($result->Data, 'Los datos deben ser una cadena (contenido del PDF en bytes).');
-        $this->assertNull($result->Message, 'El mensaje debe ser nulo.');
-        $this->assertNull($result->Errors, 'Los errores deben ser nulos.');
+
+    }
+
+    public function testObtenerPdfDteAsync_ReturnsBadRequest_WhenInvalidDataIsProvided()
+    {
+        // Arrange: Crear la solicitud PDF con datos inválidos
+        $solicitudPDF = new SolicitudDte(
+            new Credenciales(
+                rutEmisor: '', // Rut Emisor vacío (inválido)
+                nombreSucursal: 'Casa Matriz'
+            ),
+            new DteReferenciadoExterno(
+                folio: 0, // Folio inválido (debe ser mayor a 0)
+                codigoTipoDte: DTEType::FacturaElectronica,
+                ambiente: Ambiente::Certificacion
+            )
+        );
+
+        // Act: Llamar al servicio para obtener el PDF
+        $result = $this->facturacionService->ObtenerPdfDteAsync($solicitudPDF)->wait();
+
+        // Assert: Validar los resultados
+        $this->assertNotNull($result, 'El resultado no debe ser nulo.');
+        $this->assertEquals(500, $result->Status, 'El estado de la respuesta debe ser 500.');
+        $this->assertNull($result->Data, 'Los datos del PDF deben ser nulos en caso de error.');
+        $this->assertNotNull($result->Errors, 'Los errores deben contener información sobre el problema.');
+        $this->assertIsArray($result->Errors, 'Los errores deben ser un arreglo.');
     }
 }
